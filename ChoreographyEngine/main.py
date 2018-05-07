ef58656649a17ca01d919d5a313620fb8553b1ad
RenHaotian <haotian_ren@outlook.com>
2018-05-07 23:37:43 +0800

    Add the config file correctness check

--- ChoreographyEngine/main.py
+++ ChoreographyEngine/main.py
@@ -41,5 +41,45 @@ def handle_message(message: Message):
     # TODO: Need something more?
 
 
-if __name__ == '__main__':
+def check_setting():
+    from type_checker import function_type, check_type
+    right_types = [
+        ('listen_port', setting.listen_port, int),
+        ('entity_type', setting.entity_type, str),
+        ('machine_name', setting.machine_name, str),
+        ('entity_default_data', setting.entity_default_data, dict),
+        ('messages_to_receive', setting.messages_to_receive,
+         {str: (function_type, ([str], function_type, [(str, function_type, function_type)]))}),
+        ('messages_to_receive', setting.messages_to_receive, {str: (str, str)}),
+        ('machine_address', setting.machine_address, {str: str})
+    ]
+    for (setting_item_name, setting_item, right_type) in right_types:
+        if not check_type(setting_item, right_type):
+            sys.stderr.write("Config Error: `%s` does not match type `%s`.\n" %
+                             (setting_item_name, str(right_type)))
+            return False
+    if setting.entity_type != setting.machine_name:
+        sys.stderr.write("Config Error: `entity_type` is `%s` while `machine_name` is `%s`.\n" %
+                         (setting.entity_type, setting.machine_name))
+        return False
+    if setting.entity_type not in setting.machine_address:
+        sys.stderr.write("Config Error: `entity_type` `%s` is not in `machine_address` list.\n" %
+                         setting.entity_type)
+        return False
+    for message_to_receive_type in setting.messages_to_receive:
+        if message_to_receive_type not in setting.messages_paths or \
+                setting.messages_paths[message_to_receive_type][1] != setting.entity_type:
+            sys.stderr.write("Config Error: No message `%s` for `%s` to receive in `messages_paths`.\n" %
+                             (message_to_receive_type, setting.entity_type))
+            return False
+        for message_to_send_type in map((lambda x: x[0]), setting.messages_to_receive[message_to_receive_type][1][2]):
+            if message_to_send_type not in setting.messages_paths or \
+                    setting.messages_paths[message_to_send_type][0] != setting.entity_type:
+                sys.stderr.write("Config Error: No message `%s` for `%s` to send in `messages_paths`.\n" %
+                                 (message_to_send_type, setting.entity_type))
+                return False
+    return True  # Some setting content isn't been checked, including but not limited to port number and address format.
+
+
+if __name__ == '__main__' and check_setting():
     listen()
