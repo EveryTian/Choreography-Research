07ba99ec940df574f6137bf88aa570870abf2d7b
RenHaotian <haotian_ren@outlook.com>
2018-06-06 20:11:05 +0800

    Fix the module import bug

--- ChoreographyEngine/whiteboard/__init__.py
+++ ChoreographyEngine/whiteboard/__init__.py
@@ -1,4 +1,6 @@
 #! python3
 # coding: utf-8
 
-from whiteboard import WriterType, InvalidWhiteBoardMessageException, WhiteboardMessage, write_whiteboard
+from .whiteboard import WriterType, InvalidWhiteBoardMessageException, WhiteboardMessage, write_whiteboard
+
+__all__ = ['WriterType', 'InvalidWhiteBoardMessageException', 'WhiteboardMessage', 'write_whiteboard']
